0d9b1b7642dc3bc75d7b03b953b9fe0c5e99fb42
Daniar T <moonyguypie@gmail.com>
2018-11-10 13:51:48 -0700

    obfuscated

--- content.js
+++ content.js
@@ -1,7 +1,5 @@
-// var images = document.getElementsByTagName('img');
-// for (var i = 0, l = images.length; i < l; i++) {
-//   images[i].src = 'http://placekitten.com/' + images[i].width + '/' + images[i].height;
-// }
-
-document.getElementById('cs-obfuscation').replaceWith("<div id=\"content\">\n"+"<p>The ones digit of the number 47327 is 2 .</p>\n"
-	+ "<p>47327=7+2*10+3*100+2*1000+7*10000.</p>\n" + "<p>Thus there are 7 ones and so the ones digit is 7.</p>\n" + "</div>\n");
+document.getElementById("cs-obfuscation").innerHtml = this.innerHtml + "<h2>Test</h2>";
+var btn = document.createElement("BUTTON")
+var t = document.createTextNode("CLICK ME");
+btn.appendChild(t);
+document.appendChild(btn);
