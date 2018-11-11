a6b1a93c952e87c73f10134dfbe6630ab3e0e20e
Daniar T <moonyguypie@gmail.com>
2018-11-11 03:05:47 -0700

    experimental button

--- unobfuscation.js
+++ unobfuscation.js
@@ -1,4 +1,4 @@
-function httpGetAsync(aUrl, callback) {
+function httpGet(url, callback) {
   if (!window.XMLHttpRequest) {
     window.setTimeout(function() { callback(false); }, 0);
     return;
@@ -18,7 +18,7 @@ function httpGetAsync(aUrl, callback) {
     }
   }
   try {
-    xhr.open("GET", aUrl, true);
+    xhr.open("GET", url, true);
     xhr.responseType = "document";
     xhr.send();
   } catch (e) {
@@ -32,19 +32,28 @@ function httpGetAsync(aUrl, callback) {
 }
 
 var obfuscated = document.getElementsByClassName("cs-obfuscation");
+var sauce = obfuscated[0];
 
 if (!!obfuscated){
 	var pagenum = window.location.pathname.split('-');
 	var questionID = "https://textsheet.com/answer?id=" + pagenum[pagenum.length - 1].substr(1);
-	httpGetAsync(questionID, function(response) {
+	httpGet(questionID, function(response) {
 		if (!!response.getElementById("content")) {
-			obfuscated[0].innerHTML = response.getElementById("content").innerHTML;
-			obfuscated[0].style.fontSize = "xx-large";
-			obfuscated[0].className = "unobfuscation";
+			sauce.innerHTML = response.getElementById("content").innerHTML;
+			sauce.style.fontSize = "xx-large";
+			sauce.className = "unobfuscation";
 		} else {
-			obfuscated[0].innerHTML = "<span>Answer not found :(</span>";
-			obfuscated[0].style.fontSize = "xx-large";
-			obfuscated[0].className = "unobfuscation";
+			httpGet("https://textsheet.com/", function(textsheetResponse) {
+				var form = textsheetResponse.getElementsByClassName("input-group")[0];
+				form.removeChild(form.getElementsByClassName("form-control")[0]);
+				form.removeChild(form.getElementsByClassName("input-group-btn")[0]);
+				form.removeChild(form.getElementsByClassName("spacer30")[0]);
+				form = textsheetResponse.getElementById("form-buscar");
+				sauce.removeChild(sauce.getElementsByClassName("C-Eggshell-dialog C-Eggshell-inline-dialog qna sunkist-obfuscation")[0]);
+				sauce.appendChild(form);
+				sauce.style.fontSize = "xx-large";
+				sauce.className = "unobfuscation";
+			});
 		}
 	});
 }
